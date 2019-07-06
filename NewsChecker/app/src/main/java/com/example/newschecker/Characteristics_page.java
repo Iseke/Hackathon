4361c45a777c5329a53678c4b5334af699484c9a
Nursat <nursat000ktl@gmail.com>
2019-07-07 00:36:00 +0600

    seventh commit

--- NewsChecker/app/src/main/java/com/example/newschecker/Characteristics_page.java
+++ NewsChecker/app/src/main/java/com/example/newschecker/Characteristics_page.java
@@ -3,19 +3,12 @@ package com.example.newschecker;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Characteristics_page#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Characteristics_page extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,10 +51,20 @@ public class Characteristics_page extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_characteristics, container, false);
     }
